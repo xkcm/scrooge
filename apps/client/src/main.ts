4fe3f80c7ef5fe47297a00da39546a5f4cb5c567
xkcm <xkcm16@gmail.com>
2023-10-06 13:33:28 +0200

    fix(client/app): Fix pre-mature redirection

--- apps/client/src/main.ts
+++ apps/client/src/main.ts
@@ -6,13 +6,21 @@ import "./assets/styles/global.scss";
 import { router } from "./router/router";
 
 import "@scrooge/ui-library/css";
+import themeService from "./services/theme/theme.service";
+import authService from "./features/auth/auth.service";
+import fontLoaderService from "./services/font-loader/font-loader";
 
 async function initApp() {
   const vueApp = createApp(GlobalAppWrapper);
   const pinia = createPinia();
 
-  vueApp.use(router);
   vueApp.use(pinia);
+
+  await authService.resolveAuthState();
+  themeService.initTheme();
+  await fontLoaderService.loadFonts();
+
+  vueApp.use(router);
   vueApp.mount("#app");
 }
 
