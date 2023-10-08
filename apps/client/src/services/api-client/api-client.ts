c48a306b98b549c0fe1eae3e382eccbd60b3d00c
xkcm <xkcm16@gmail.com>
2023-10-09 00:45:13 +0200

    fix(client/app): Refactor typing on api-client

--- apps/client/src/services/api-client/api-client.ts
+++ apps/client/src/services/api-client/api-client.ts
@@ -3,8 +3,6 @@ import * as user from "./modules/api-client.user.module.js";
 import * as session from "./modules/api-client.session.module.js";
 import * as operation from "./modules/api-client.operation.module.js";
 
-// todo: refactor this api client
-// - [ ] use schema types from @scrooge/shared
 const apiClient = {
   auth,
   user,
